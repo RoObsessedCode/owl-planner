ce55fda139ded663ac1685eee11a4bc18854b1a3
Rohan Saigal <rohan4saigal@gmail.com>
2018-01-31 20:41:09 -0500

    add(index.html): add weather icon cdn

---
+++ client/src/Weather.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { GenericWeather } from 'react-weather'
+
+const Weather = (props) => {
+  return (
+    <GenericWeather city="Manhattan" />
+  )
+}
+
+export default Weather
